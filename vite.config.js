3a8734020bae68774719a462004a6b0e07593bb1
cswebdev <cswebdev91@gmail.com>
2025-09-15 20:32:56 -0400

    created test page for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  base: '/optical-calculator/',
   plugins: [react(), tsconfigPaths()]
 })
