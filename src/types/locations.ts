f7d9e828bff9732e56460fa6daa29fcf99ba054c
cswebdev <csnider91@live.com>
2026-02-20 13:19:29 -0500

    update package.json, added seneca to store locations, organized store locations alphabetically

--- src/types/locations.ts
+++ src/types/locations.ts
@@ -4,11 +4,12 @@ export type StoreLocation = {
 };
 
 export const storeLocations: StoreLocation[] = [
-    { name: "Clemson", taxRate: 0.07 },
-    { name: "Greenville", taxRate: 0.06 },
-    { name: "Simpsonville", taxRate: 0.06 },
-    { name: "Easley", taxRate: 0.07 },
     { name: "Anderson", taxRate: 0.07 },
+    { name: "Clemson", taxRate: 0.07 },
     { name: "Clinton", taxRate: 0.08 }, 
+    { name: "Easley", taxRate: 0.07 },
+    { name: "Greenville", taxRate: 0.06 },
+    { name: "Seneca", taxRate: 0.06},
+    { name: "Simpsonville", taxRate: 0.06 },
     // Add more locations as needed
 ];
